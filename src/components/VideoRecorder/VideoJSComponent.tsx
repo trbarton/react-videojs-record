441204a3a0812cc3273321d6fc3beb202457b329
Tom Barton <tom@paranimo.co.uk>
2023-05-03 14:14:51 +0100

    Added video recorder component

---
+++ src/components/VideoRecorder/VideoJSComponent.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import videojs from "video.js";
+import "video.js/dist/video-js.css";
+
+import "webrtc-adapter";
+import RecordRTC from "recordrtc";
+
+// register videojs-record plugin with this import
+import "videojs-record/dist/css/videojs.record.css";
+
+// eslint-disable-next-line no-unused-vars
+import Record from "videojs-record";
+import { useEffect, useRef, version } from "react";
+
+export const VideoJSComponent = (props: any) => {
+  const videoRef = useRef<any>(null);
+  const playerRef = useRef<any>(null);
+  const { options, onReady } = props;
+
+  useEffect(() => {
+    // Make sure Video.js player is only initialized once
+    if (!playerRef.current) {
+      // The Video.js player needs to be _inside_ the component el for React 18 Strict Mode.
+      const videoElement = document.createElement("video-js");
+
+      videoElement.className = "video-js vjs-default-skin";
+      videoRef.current.appendChild(videoElement);
+
+      const player = (playerRef.current = videojs(videoElement, options, () => {
+        // print version information at startup
+        const version_info =
+          "Using video.js " +
+          videojs.VERSION +
+          " with videojs-record " +
+          videojs.getPluginVersion("record") +
+          ", recordrtc " +
+          RecordRTC.version +
+          " and React " +
+          version;
+        videojs.log(version_info);
+
+        onReady && onReady(player);
+      }));
+
+      // You could update an existing player in the `else` block here
+      // on prop change
+    } else {
+      // const player = playerRef.current;
+      // player.record().getDevice();
+    }
+  }, [options, videoRef]);
+
+  // Dispose the Video.js player when the functional component unmounts
+  useEffect(() => {
+    const player = playerRef.current;
+
+    return () => {
+      if (player && !player.isDisposed()) {
+        player.dispose();
+        playerRef.current = null;
+      }
+    };
+  }, [playerRef]);
+
+  return (
+    <div data-vjs-player>
+      <div ref={videoRef} />
+    </div>
+  );
+};
+
+export default VideoJSComponent;
---
+++ src/components/VideoRecorder/Wrapper.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+import "./VideoRecorder.css";
+import VideoJSComponent from "./VideoJSComponent";
+import { useRef } from "react";
+
+export default function Wrapper() {
+  const playerRef = useRef(null);
+  const videoJsOptions = {
+    controls: true,
+    bigPlayButton: false,
+    width: 320,
+    height: 240,
+    fluid: false,
+    plugins: {
+      /*
+      // wavesurfer section is only needed when recording audio-only
+      wavesurfer: {
+        backend: 'WebAudio',
+        waveColor: '#36393b',
+        progressColor: 'black',
+        debug: true,
+        cursorWidth: 1,
+        msDisplayMax: 20,
+        hideScrollbar: true,
+        displayMilliseconds: true,
+        plugins: [
+          // enable microphone plugin
+          WaveSurfer.microphone.create({
+            bufferSize: 4096,
+            numberOfInputChannels: 1,
+            numberOfOutputChannels: 1,
+            constraints: {
+              video: false,
+              audio: true
+            }
+          })
+        ]
+      },
+      */
+      record: {
+        audio: true,
+        video: true,
+        maxLength: 10,
+        debug: true,
+      },
+    },
+  };
+
+  const handlePlayerReady = (player: any) => {
+    playerRef.current = player;
+
+    // handle player events
+    // device is ready
+    player.on("deviceReady", () => {
+      console.log("device is ready!");
+    });
+
+    // user clicked the record button and started recording
+    player.on("startRecord", () => {
+      console.log("started recording!");
+    });
+
+    // user completed recording and stream is available
+    player.on("finishRecord", () => {
+      // recordedData is a blob object containing the recorded data that
+      // can be downloaded by the user, stored on server etc.
+      console.log("finished recording: ", player.recordedData);
+    });
+
+    // error handling
+    player.on("error", (element: any, error: any) => {
+      console.warn(error);
+    });
+
+    player.on("deviceError", () => {
+      console.error("device error:", player.deviceErrorCode);
+    });
+  };
+
+  return (
+    <div className="App">
+      <VideoJSComponent options={videoJsOptions} onReady={handlePlayerReady} />
+    </div>
+  );
+}
